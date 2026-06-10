813c086671258fba04aba40d0f85268e209fb3d2
sadique786 <sadiqueulhussain786@gmail.com>
2026-06-11 01:28:34 +0530

    Milestone 1.2

---
+++ src/datasets/base_dataset.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class BaseDataset(ABC):
+
+    @abstractmethod
+    def get_train_dataset(self):
+        pass
+
+    @abstractmethod
+    def get_test_dataset(self):
+        pass
+
+    @abstractmethod
+    def num_classes(self):
+        pass
